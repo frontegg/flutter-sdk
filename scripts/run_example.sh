ba4af7e87a4d7f6da0dab9ad7ae403cfb518d3dc
isysenko <sip31296@gmail.com>
2021-12-29 16:04:37 +0200

    update dart.yml

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -5,9 +5,9 @@ for dir in */ ; do
     cd ${dir}
     pwd
     flutter clean
-    flutter build apk
+    flutter build apk --no-sound-null-safety
     flutter clean
-    flutter build ios --release
+    flutter build ios --no-sound-null-safety
     cd ..
     pwd
     if [ "$#" -gt 0 ]; then shift; fi
